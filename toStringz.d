7173751a9c978d99263920b2fdf0c6f701cf23a0
Jacob Carlborg <doob@me.com>
2018-01-04 16:31:09 +0100

    Add toStringz example

---
+++ toStringz.d
@@ -0,0 +1,38 @@
+// import std.stdio : println = writeln;
+
+import core.stdc.stdlib : alloca;
+import core.stdc.stdio : printf;
+
+// extern (C) void* _d_arraycopy;
+
+enum toStringzMaxLength = 5;
+
+immutable(char)* toStringz(in string str,
+    void* buf = alloca(toStringzMaxLength + 1)) pure
+{
+    enum maxLength = toStringzMaxLength;
+
+    if (str.length == 0)
+        return "".ptr;
+
+    immutable pointer = str.ptr + str.length;
+
+    if ((cast(size_t) pointer & 3) && *pointer == 0)
+        return &str[0];
+
+    auto len = str.length > maxLength ? maxLength : str.length;
+    char[] buffer = cast(char[]) buf[0 .. len + 1];
+    buffer[0 .. len] = str[0 .. len];
+    buffer[len] = '\0';
+    debug printf("aaaaaaaaaaaaaaaaaaa\n");
+    return buffer[0 .. len + 1].ptr;
+}
+
+extern (C) int main(int c, char** v)
+{
+    string s = "123456";
+    auto a = s.toStringz;
+    printf("%s\n", a);
+    return 0;
+}
+
